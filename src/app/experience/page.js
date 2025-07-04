18a0d5c6e0640f565f6b94a172980889f127e07e
Kery <Keryseverino@gmail.com>
2025-07-04 16:01:10 -0400

    Added a new page that displays my journey

---
+++ src/app/experience/page.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+import ExperienceComponent from "../components/ExperienceComponent";
+
+export default function experience() {
+    
+  return (
+    <main>
+        <Navbar />
+        <ExperienceComponent />
+    </main>
+  );
+}
